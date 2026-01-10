8a03773fbfa3d45688250653c172ed4ea526658c
Teddy <teddybrian543@gmail.com>
2026-01-10 10:04:06 +0300

    Fix Navbar and Footer Positions

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import Link from "next/link";
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Footer from "./components/Footer"; 
 
 export default function Home() {
   
 
   return (
     <main className="min-h-screen bg-gray-900 text-white">
-      <Navbar />
       <Hero />
-      <Footer />
     </main>
   );
 }
